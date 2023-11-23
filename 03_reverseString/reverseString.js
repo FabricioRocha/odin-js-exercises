f89cf0d05496008bde0302af5e54dffc384e6f0c
Fabricio Rocha <FabricioRocha@users.noreply.github.com>
2023-11-23 18:57:54 -0300

    Excs 3 and 4 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(original) {
+    let newstr = "";
+    let strindex = original.length - 1;
 
+    while (strindex >= 0) {
+        newstr += original[strindex--];
+    }
+
+    return newstr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    let newarr = [];
+    for (const arrItem of arr) {
+        if (toRemove.indexOf(arrItem) < 0) {
+            newarr.push(arrItem)
+        }
+    }
 
+    return newarr;
 };
 
 // Do not edit below this line
