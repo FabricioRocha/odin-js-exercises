7565c77bd64436cdfe57a5a13b2f70081cb44b18
Fabricio Rocha <FabricioRocha@users.noreply.github.com>
2023-11-23 17:14:00 -0300

    excs 1 and 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    
+    if (isNaN(num) || num < 0) return 'ERROR';
+    
+    let newstr = "";
+    
+    for (let i = 0; i < num; i++)
+        newstr += str;
 
+    return newstr;
 };
 
 // Do not edit below this line
