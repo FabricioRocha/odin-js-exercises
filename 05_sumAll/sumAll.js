6b67c2d8c91b9b08b3639640782d1d283d6efcb8
Fabricio Rocha <FabricioRocha@users.noreply.github.com>
2023-11-24 11:57:14 -0300

    Exercise 05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(bottom, top) {
+    if (Number.isNaN(bottom) || Number.isNaN(top) ||
+        typeof (bottom) != "number" || typeof(top) !== "number" ||
+        bottom < 0 || top < 0) {
+        return 'ERROR';
+    }
 
+    let running = 0;
+    if (bottom <= top) {
+        for (let i = bottom; i <= top; i++) running += i;
+    } else if (bottom >= top) {
+        for (let i = top; i <= bottom; i++) running += i;
+    }
+    
+    return running;
 };
+
+
+// teste
+// let t = sumAll (4,1);
 
 // Do not edit below this line
 module.exports = sumAll;
