dcc78e080d8ee781e04876bd086da6d6312e337e
Fabricio Rocha <FabricioRocha@users.noreply.github.com>
2023-11-26 23:06:20 -0300

    Exercise 10 (Fibonacci) complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+    let index = parseInt(pos) - 1;
+    if (index < 0) return "OOPS";
+    if (index === 0) return 0;
+
+    let arrSeq = [1, 1];
+
+    if (index < arrSeq.length) {
+        return arrSeq[index];
+    } else {
+        for (let i = arrSeq.length; i <= index; i++) {
+            arrSeq.push(arrSeq[i-1] + arrSeq[i-2]);
+        }
+        return arrSeq[arrSeq.length - 1];
+    }
 
 };
 
