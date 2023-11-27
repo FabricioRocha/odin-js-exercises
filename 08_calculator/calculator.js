ef3604ff7e5f44d5bec7bcb7e71bb359405bbd39
Fabricio Rocha <FabricioRocha@users.noreply.github.com>
2023-11-26 21:50:15 -0300

    Exercise 08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(...args) {
+	 let values = args.flat();
+   let total = 0;
+   for (val of values) total += val;
+   return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(minuend, subtraend) {
+	return minuend - subtraend;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  let values = args.flat();
+  let total = 0;
+  for (val of values) total += val;
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let values = args.flat();
+  let total = 0;
+  if (args.length > 0 ) {
+    total = 1;
+    for (val of values) total*=val;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	if (exp === 0) return 1;
+  let res = 1;
+  for (let i = 1; i<=exp; i++) res*=base;
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+	if (val === 0) return 1;
+  let res = val;
+  for (let i = val - 1; i >= 1; i--) res *= i;
+  return res;
 };
 
 // Do not edit below this line
