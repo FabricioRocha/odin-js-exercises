c78f1e747523ab97076d5ffc9992ff608f293cae
Fabricio Rocha <FabricioRocha@users.noreply.github.com>
2023-11-26 22:26:06 -0300

    Exercise 09 (Palindromes) complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let rechars = /([0-9]|[a-z]|[A-Z]){1}/
+    // Prepare original string
+    let original = "";
+    for (let i = 0; i < str.length; i++ ) {
+        if (str[i].match(rechars)) original += str[i].toLowerCase();
+    }
 
+    // Prepare reverse string
+    let reverse = "";
+    for (let i = original.length - 1; i >= 0; i--) {
+        reverse += original[i];
+    }
+
+    // Compare the prepared strings
+    let flag = true;
+    for (let i = 0; i < original.length; i++) {
+        if (original[i] !== reverse[i]) {
+            flag = false;
+            break;
+        }
+    }
+
+    return flag;
 };
 
 // Do not edit below this line
