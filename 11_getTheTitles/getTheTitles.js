2b4c221f602a36c875989d96272b628dbd603be2
Fabricio Rocha <FabricioRocha@users.noreply.github.com>
2023-11-27 22:28:12 -0300

    Exercises 11 and 12 complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(objarr) {
+    let titles = [];
+    for (item of objarr) titles.push(item.title);
+    return titles;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    let curtime = new Date();
+    let thisYear = curtime.getFullYear();
+    let eldest;
 
+    if (peopleArr.length < 1) return {};
+
+    for (person of peopleArr) {
+        let personAge = (person.yearOfDeath || thisYear) - person.yearOfBirth;
+        
+        if (eldest === undefined || (personAge > (eldest.yearOfDeath || thisYear) - eldest.yearOfBirth)) {
+            eldest = person;
+        }
+    }
+
+    return eldest;
 };
 
 // Do not edit below this line
